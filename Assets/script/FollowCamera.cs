e0bcdb92466c90393d33de4754ac4c2b8813d974
laventhyst <korawan.aphi@bumail.net>
2025-03-06 18:11:40 +0700

    airplane

---
+++ Assets/script/FollowCamera.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float smoothSpeed = 2f;
+    [SerializeField] Vector3 offset = new Vector3(0, 2, -5);
+
+    private void LateUpdate()
+    {
+        Vector3 desiredPosition = target.position = target.TransformDirection(offset);
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        transform.LookAt(target);
+    }
+}
